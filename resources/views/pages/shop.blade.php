938c2c804e7a7de715c7aec657f1a0e422965406
EMMANUEL ATSU <zioncodex@gmail.com>
2023-09-17 23:48:20 +0100

    commit

--- resources/views/pages/shop.blade.php
+++ resources/views/pages/shop.blade.php
@@ -12,14 +12,16 @@
     
         @include('components/inc_main_slide_banner_section_shop')
 
+        @include('components/inc_shop_features_section_shop')
+
+        @include('components/inc_featured_products_section_shop')
+
         @include('components/inc_category1_section_shop')
 
         @include('components/inc_category2_section_shop')
 
         @include('components/inc_category3_section_shop')
 
-        @include('components/inc_shop_features_section_shop')
-   
         @include('components/inc_brand_section_shop')
 
         <!---------------------------------------------------------->
