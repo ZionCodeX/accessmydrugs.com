412294991a3ed0578d811d33b39aeab1ef46e28d
EMMANUEL ATSU <zioncodex@gmail.com>
2024-05-12 11:44:12 +0100

    Email template Update

--- resources/views/layouts/base_email.blade.php
+++ resources/views/layouts/base_email.blade.php
@@ -133,11 +133,11 @@ width: 100% !important;
                     <td class="content-cell" align="center" style="box-sizing: border-box; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; position: relative; max-width: 100vw; padding: 32px;">
 
                         <a href="https://www.procurement.spreaditglobal.com" class="brand-logo">
-							<img alt="Logo" src="{{ url('assets/media/logos/spreadit-logo2.png'); }}" height="45px" />
+							<img alt="Logo" src="{{ url('assets/media/img/logo/logo.png'); }}" height="45px" />
 						</a>
 
                         <p style="box-sizing: border-box; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; position: relative; line-height: 1.5em; margin-top: 0; color: #b0adc5; font-size: 12px; text-align: center;">
-                            © 2021 Spreadit Limited. All rights reserved.
+                            © 2024 AccessMyDrugs. All rights reserved.
                         </p>
 
                     </td>
