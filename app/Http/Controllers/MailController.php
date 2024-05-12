3122647bce44d565ff97262c0f4c1b0d6a47854f
EMMANUEL ATSU <zioncodex@gmail.com>
2024-05-12 14:14:51 +0100

    Commit

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -23,7 +23,7 @@ class MailController extends Controller
             $data['mail_template'] = $xdata["mail_template"];
         
             //send mail
-            \Mail::to($data["to"])->send(new SendMail($data));
+            //\Mail::to($data["to"])->send(new SendMail($data));
 
             //check and respond to mail request
             if (\Mail::to($data["to"])->send(new SendMail($data))) 
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,39 +3,22 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Auth\Notifications\VerifyEmail;
-use Illuminate\Notifications\Messages\MailMessage;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
-     *
-     * @return void
      */
-    public function register()
+    public function register(): void
     {
         //
     }
 
     /**
      * Bootstrap any application services.
-     *
-     * @return void
      */
-    public function boot()
+    public function boot(): void
     {
         //
-        return (new MailMessage)->view(
-            'emails.registration_email', ['order' => 555]
-            );
-        /** 
-        VerifyEmail::toMailUsing(function ($notifiable, $url) {
-            return (new MailMessage)
-                ->subject('Verify Email AddressX')
-                ->line('Click the button below to verify your email address.')
-                ->action('OK Verify Email Address', $url);
-        });
-        */
     }
 }
